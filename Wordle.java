64dc9bcd2f7b9f8fdabddd1be4ae92a43a773383
נטע כנרי <netakanary@MacBook-Pro-sl-nt.local>
2025-11-30 22:11:41 +0200

    wordle2

--- Wordle.java
+++ Wordle.java
@@ -132,7 +132,8 @@ public class Wordle {
         }
 
         if (!won) {
-            // ... follow the assignment examples for how the printing should look like
+            System.out.println("Sorry, you did not guess the word.");
+            System.out.println("The secret word was: " + secret);
         }
 
         inp.close();
